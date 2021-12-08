eb831dbc92b73ff1d603a7cf5e9066cf06353f54
DmitrenkoAlex <dmitrenkolesha@mail.ru>
2021-12-08 04:12:46 +0300

    Rename V2.0

--- src/main/java/ru/netology/sqr/SqrService.java
+++ src/main/java/ru/netology/sqr/SqrService.java
@@ -1,10 +1,10 @@
 package ru.netology.sqr;
 
 public class SqrService {
-    public int sqr(int a, int b) {
+    public int sqr(int lowerBound, int upperBound) {
         int iterrationAmount = 0;
         for (int i = 10; i >= 10 && i <= 99; i++) {
-            if (i * i <= b && i * i >= a) {
+            if (i * i <= upperBound && i * i >= lowerBound) {
                 iterrationAmount++;
 
             }
